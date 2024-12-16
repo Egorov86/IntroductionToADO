7a25542f88ae30fe45ab5685ca55b5fbd444e3b7
Egoro_st25@top-academy.ru <Egoro_st25@top-academy.ru>
2024-12-16 21:03:24 +0300

    SELECT in Library2 DONE

---
+++ ConsoleApp1/Library2/Library.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.ComponentModel.Design;
+
+namespace Library2
+{
+    internal class Library
+    {
+        static string connectionString = "";
+        public static SqlConnection connection = null;
+        static readonly string delimiter1 = "\n------------------------------------\n";
+        static readonly string delimiter2 = "\n=====================================\n";
+        static Library()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["Library"].ConnectionString;
+            connection = new SqlConnection(connectionString);
+            Console.WriteLine(connectionString);
+        }
+        public static void Select(string fields, string tables, string condition = "", int padding=20)
+        {
+            string cmd = $"SELECT {fields} FROM {tables}";
+            if (condition.Length > 0) cmd += $" WHERE {condition}";
+            cmd += ";";
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                Console.WriteLine(delimiter1);
+                for (int i = 0; i < reader.FieldCount; i++)
+                    Console.Write(reader.GetName(i).PadRight(padding));
+                Console.WriteLine();
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write(reader[i].ToString().PadRight(padding));
+                    Console.WriteLine();
+                }
+                Console.WriteLine(delimiter2);
+            }
+            reader.Close();
+            connection.Close();
+        }
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+    }
+}
---
+++ ConsoleApp1/Library2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library2
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Library.Select("author_id,first_name,last_name", "Authors");
+            Library.Select("book_title,publish_date,[Author]=first_name+' '+last_name",
+                "Books,Authors",
+                "author=author_id", 32
+                );
+        }
+    }
+}
